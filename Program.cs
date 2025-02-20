bdb481dacddb35046caf0950d2d50ccc5e408c51
Cynder Klim <cklim@my.wctc.edu>
2025-02-19 23:28:22 -0600

    [1.08] Create parallel lists of character details

--- Program.cs
+++ Program.cs
@@ -14,6 +14,15 @@ if (!File.Exists(file))
 }
 else
 {
+    // create parallel lists of character details
+    // lists are used since we do not know number of lines of data
+    List<UInt64> Ids = [];
+    List<string> Names = [];
+    List<string> Descriptions = [];
+    List<string> Species = [];
+    List<string> FirstAppearance = [];
+    List<UInt64> YearCreated = [];
+
     string? choice;
     do
     {
