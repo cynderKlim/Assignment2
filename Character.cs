af52b11fc2c0435d56e963b96ebbf23c58179412
Cynder Klim <cklim@my.wctc.edu>
2025-02-27 12:59:43 -0600

    [2.011] Updated Character class

--- Character.cs
+++ Character.cs
@@ -3,9 +3,12 @@ class Character
     public UInt64 Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
+    public string Species { get; set; } = string.Empty;
+    public string FirstAppearance { get; set; } = string.Empty;
+    public string YearCreated { get; set; } = string.Empty;
 
     public string Display()
     {
-        return $"Id: {Id}\nName: {Name}\nDescription: {Description}\n";
+        return $"Id: {Id}\nName: {Name}\nDescription: {Description}\nSpecies: {Species}\nFirst Appearance: {FirstAppearance}\nYear Created: {YearCreated}\n";
     }
 }
